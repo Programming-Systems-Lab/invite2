e2f22304216c0ad37be9c485b954fbc0bb241fc3
Nikhil Sarda <nikhilsarda.iitkgp@gmail.com>
2012-07-27 13:02:21 -0400

    Fixed invokestatic bug thanks to Jon.

--- src/edu/columbia/cs/psl/invivo/runtime/Test.java
+++ src/edu/columbia/cs/psl/invivo/runtime/Test.java
@@ -2,7 +2,20 @@ package edu.columbia.cs.psl.invivo.runtime;
 
 public class Test {
 
+	static int b;
+	
+	static boolean[] a = {true};
+	
+	static int a_fill = 0;
+	
+	static String g() {
+		return "";
+	}
+	
 	public static void main(int x, int y) throws InterruptedException {
-		Integer a = new Integer(x + y);
+		
+		boolean b;
+		g();
+		b = a[a_fill++];
 	}
 }
